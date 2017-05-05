a4e26b9df5ddc3ed9462cf1be7ded20aa76634ca
Shane Andreasen <shane@bravadohealth.com>
2017-05-04 20:37:18 -0400

    "Add new patient" updates
    Made some additions and changes to the “add new patient” pages.
    
    Set up PHP to dynamically fill content for UI testing purposes.

--- rx/add-new-patient-filled.php
+++ rx/add-new-patient-filled.php
@@ -1,35 +1,35 @@
 <?php
-$firstName = "";
-$middleName = "";
-$lastName = "";
+$firstName = "William";
+$middleName = "Woolworth";
+$lastName = "Wallace";
 $fullName = "$firstName $middleName $lastName";
 $address = "";
-$city = "";
-$state = "";
-$zipCode = "";
-$birthday = "";
-$gender = "";
-$weight = "";
-$email = "";
-$phone = "";
-$allergies = "";
-$homemeds = "";
-$diagnoses = "";
+$city = "Dunedin";
+$state = "FL";
+$zipCode = "34698";
+$birthday = "4/3/1270";
+$gender = "Male";
+$weight = "184";
+$email = "williamwallace@freedom.scot";
+$phone = "(555) 555-5785";
+$allergies = "Tetracycline, Penicillin, NSAIDs";
+$homemeds = "None";
+$diagnoses = "None";
 ?>
 
 <?php include "add-new-patient-body.php"; ?>
 					<div class="column lg-6 app-right">
 						<div class="flow-section">
 							<h2 class="section-title">New Patient Confirmation</h2>
-							<div class="verify-info empty">
-								<h3 class="header">Patient details are displayed here</h3>
+							<div class="verify-info">
+								<h3 class="header"><?php echo $fullName ?></h3>
 								<div class="controls-footer clearfix">
 									<div class="button-group flow-controls pull-right">
 										<button type="button" role="link" class="button button-link secondary-action">
 											<a href="../home/home-view.php">Cancel</a>
 										</button>
-										<button type="button" role="link" class="button button-flat primary-action disabled">
-											<a href="add-new-patient-filled.php">Add patient…</a>
+										<button type="button" role="link" class="button button-flat primary-action">
+											<a href="">Add patient…</a>
 										</button>
 									</div>
 								</div>
